f591021249f348635d92af17167305a6254590d1
Andrew Korneev <korniandr@gmail.com>
2021-12-30 11:42:25 +0300

    burger

--- js/typography.js
+++ js/typography.js
@@ -1,37 +1,7 @@
-/* Устанавливаем индекс слайда по умолчанию */
-let slideIndex = 1;
-showSlides(slideIndex);
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.burger-menu');
 
-/* Увеличиваем индекс на 1 — показываем следующий слайд*/
-function nextSlide() {
-    showSlides(slideIndex += 1);
-}
-
-/* Уменьшает индекс на 1 — показываем предыдущий слайд*/
-function previousSlide() {
-    showSlides(slideIndex -= 1);  
-}
-
-/* Устанавливаем текущий слайд */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-/* Функция перелистывания */
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slider__item");
-    
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-  
-  /* Проходим по каждому слайду в цикле for */
-    for (let slide of slides) {
-        slide.style.display = "none";
-    }   
-    slides[slideIndex - 1].style.display = "block"; 
-}
+menuBtn.addEventListener('click', function(e){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
