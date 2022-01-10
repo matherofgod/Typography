c3bbf142639935e555dec247a1b5bfc7d365358f
Andrew Korneev <korniandr@gmail.com>
2022-01-10 23:51:27 +0300

    modal window

--- js/typography.js
+++ js/typography.js
@@ -6,3 +6,30 @@ menuBtn.addEventListener('click', function(e){
 	menu.classList.toggle('active');
 })
 
+
+ //  окно
+ var modal = document.getElementById("orderModal");
+
+ // кнопка открытия окна
+ var btn = document.getElementById("orderBtn");
+ 
+ //  <span> элемент закрывающий окно
+ var span = document.getElementsByClassName("close")[0];
+ 
+ // открытие окна при нажатии кнопки
+ btn.onclick = function() {
+   modal.style.display = "block";
+ }
+ 
+ // при клике <span> (x), закрыть окно
+ span.onclick = function() {
+   modal.style.display = "none";
+ }
+ 
+ // при клике за пределами окна, окно закрывается
+ window.onclick = function(event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ }
+
